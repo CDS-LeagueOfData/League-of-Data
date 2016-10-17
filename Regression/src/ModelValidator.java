e2b1d82ecc78e0d3870d66cc5b82b0d9270d3de7
fufjvnvnf <fufjvnvnf@gmail.com>
2016-10-17 13:57:52 -0400

    added the rest of cleans to test

--- Regression/src/ModelValidator.java
+++ Regression/src/ModelValidator.java
@@ -1,29 +1,31 @@
 
 public class ModelValidator {
-	
-	public static void main(String[] args){
-		String[] files = {"david-clean-1.json","david-clean-2.json","david-clean-3.json","david-clean-4.json",
-		                  "david-clean-5.json","david-clean-6.json","david-clean-7.json","david-clean-8.json"};
-		String[] params = {"kills","deaths","assists","goldEarned","minionsKilled"};
-		
-		for (int n = 2; n < files.length; n++){
+
+	public static void main(String[] args) {
+		String[] files = { "david-clean-1.json", "david-clean-2.json", "david-clean-3.json", "david-clean-4.json",
+				"david-clean-5.json", "david-clean-6.json", "david-clean-7.json", "david-clean-8.json",
+				"david-clean-9.json", "sam-clean-1.json", "sam-clean-2.json", "sam-clean-3.json", "sam-clean-4.json",
+				"wilson-clean-1.json" };
+		String[] params = { "kills", "deaths", "assists", "goldEarned", "minionsKilled" };
+
+		for (int n = 2; n < files.length; n++) {
 			double res = nFold(n, files, params);
 			System.out.println(n + "-fold validation: " + res);
 		}
 	}
-	
-	public static double nFold(int n, String[] files, String[] params){
-		String[] testSet = new String[files.length/n];
+
+	public static double nFold(int n, String[] files, String[] params) {
+		String[] testSet = new String[files.length / n];
 		String[] trainingSet = new String[files.length - testSet.length];
-		
+
 		double sum = 0;
 		// run n times total
-		for(int iteration = 0; iteration < n; iteration ++) {
+		for (int iteration = 0; iteration < n; iteration++) {
 			// distribute the files into test and training sets
 			int testSetInd = 0;
 			int trainingSetInd = 0;
-			for(int i = 0; i < files.length; i++){
-				if( (i+iteration)%n == 0){
+			for (int i = 0; i < files.length; i++) {
+				if ((i + iteration) % n == 0) {
 					testSet[testSetInd] = files[i];
 					testSetInd++;
 				} else {
@@ -31,26 +33,26 @@ public class ModelValidator {
 					trainingSetInd++;
 				}
 			}
-			
+
 			// run the regression
 			ParseJson trainJSON = new ParseJson(trainingSet, params);
-			double[][] values  = trainJSON.getValues();
-			double[]   ratings = trainJSON.getRatings();
-			
-			// 	run the regression on trainingSet to get coefficients
+			double[][] values = trainJSON.getValues();
+			double[] ratings = trainJSON.getRatings();
+
+			// run the regression on trainingSet to get coefficients
 			double[][] coefficients = LinearRegression.approximateRatingCoef(values, ratings);
-			
+
 			// test on testSet
 			ParseJson testJSON = new ParseJson(testSet, params);
-			double[][] testValues  = testJSON.getValues();
-			double[]   realRatings = testJSON.getRatings();
-			
-			double[][] predictions = LinearRegression.predict(testValues,  coefficients);
-			for(int i = 0; i < testSet.length; i++){
-				sum += Math.abs( realRatings[i] - predictions[i][0] );
+			double[][] testValues = testJSON.getValues();
+			double[] realRatings = testJSON.getRatings();
+
+			double[][] predictions = LinearRegression.predict(testValues, coefficients);
+			for (int i = 0; i < testSet.length; i++) {
+				sum += Math.abs(realRatings[i] - predictions[i][0]);
 			}
 		}
-		
-		return sum/n;
+
+		return sum / n;
 	}
 }
