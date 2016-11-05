872cbaf19c361720d11a27344545f12c54686532
Zhao, David <dfz8@cornell.edu>
2016-11-05 14:47:07 -0400

    optimizer stub

---
+++ Regression/src/ModelOptimizer.java
@@ -0,0 +1,31 @@
+
+public class ModelOptimizer {
+	
+	static final double PENALTY = 10.0;
+	static final double THRESHOLD = 0.7;
+	
+	static class Model{
+		String[] params;
+		double score;		
+	}
+	
+	public static void main(String[] args){
+	
+		//TODO: maybe move to a method if necessary
+		String[] allParams; 
+	}
+	
+	public static Model optimize(){
+		return null;
+	}
+	
+	
+	public static double calculateScore(String[] params){
+		return 0.0;
+	}
+
+	public static boolean passCorrelationCheck(String param, Model model){
+		return false;
+	}
+	
+}
