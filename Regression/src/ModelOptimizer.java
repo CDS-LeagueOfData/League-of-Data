129cdef59805b8e8227efdc145bfc875aea62474
Zhao, David <dfz8@cornell.edu>
2016-11-12 00:13:36 -0500

    model

--- Regression/src/ModelOptimizer.java
+++ Regression/src/ModelOptimizer.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 
 public class ModelOptimizer {
 	
@@ -6,7 +8,11 @@ public class ModelOptimizer {
 	
 	static class Model{
 		String[] params;
-		double score;		
+		double score;
+		public Model(String[] p, double s){
+			params = p;
+			score = s;
+		}
 	}
 	
 	public static void main(String[] args){
@@ -15,8 +21,24 @@ public class ModelOptimizer {
 		String[] allParams; 
 	}
 	
-	public static Model optimize(){
-		return null;
+	public static Model optimize(String[] p){
+		LinkedList<String> available = new LinkedList<String>();
+		for(String s: p)
+			available.add(s);
+		
+		Model bestModel = new Model(new String[0], Double.MAX_VALUE);
+		boolean complete = false;
+		while(!complete){
+			// pick next parameter to include
+			String testP = "";
+			
+			if(passCorrelationCheck(testP, bestModel)){
+			}
+			if(available.size() == 0){
+				complete = true;
+			}
+		}
+		return bestModel;
 	}
 	
 	
