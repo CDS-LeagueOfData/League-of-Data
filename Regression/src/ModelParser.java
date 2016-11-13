df45b6669244d1cf1caebb0c12223972f63084ba
fufjvnvnf <fufjvnvnf@gmail.com>
2016-11-13 17:34:38 -0500

    almost done with rater. need sam's method for pulling data

--- Regression/src/ModelParser.java
+++ Regression/src/ModelParser.java
@@ -1,4 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ModelParser {
-
+	public static Model parseModel() {
+		try {
+			BufferedReader bf = new BufferedReader(new FileReader(new File("model.txt")));
+			bf.readLine(); // first line indicates param length
+			String line;
+			ArrayList<String> params = new ArrayList<String>();
+			ArrayList<Double> coef = new ArrayList<Double>();
+			while ((line = bf.readLine()) != null) {
+				String[] comp = line.split(" ");
+				params.add(comp[0]);
+				coef.add(Double.parseDouble(comp[1]));
+			}
+			double[] c = new double[params.size()];
+			for (int i = 0; i < coef.size(); i++) {
+				c[i] = coef.get(i);
+			}
+			return new Model(params, c);
+		} catch (FileNotFoundException e) {
+			System.err.println("No model file found");
+			System.exit(1);
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
