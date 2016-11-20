b7f28f16bc314a7b699c68c89900e8d343e36347
fufjvnvnf <fufjvnvnf@gmail.com>
2016-11-20 14:59:08 -0500

    more data

--- Regression/src/MatchRater.java
+++ Regression/src/MatchRater.java
@@ -1,10 +1,7 @@
-import java.util.Scanner;
-
 import com.google.gson.JsonObject;
 
 public class MatchRater {
 	
-	@SuppressWarnings("resource")
 	public void main(String[] args) {
 		JsonObject js = MatchCleaner.getCleanData();
 		Model m = ModelParser.parseModel();
